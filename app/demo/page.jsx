b5c389d1217b5c2656076fef13d5048ca2ff3c41
jimbits <jimbits@jimhortons.com>
2024-04-04 17:59:58 -0600

    to do list RTDB data added

--- app/demo/page.jsx
+++ app/demo/page.jsx
@@ -1,15 +1,27 @@
 import {ToDoList} from '@/components/todos/ToDoList'
 import {AddTask} from '@/components/dialogs/AddTask'
 import {ToDoListItem} from '@/components/todos/ToDoListItem'
-function DemoPage({children}) {
+
+import {getToDoItems} from '@/lib/firebase/api'
+
+async function DemoPage({children}) {
+	//  call our firebase data...
+	// RTDB SDK: Object.keys, Object.values, Object.entries  {task:{dfjlsdjfldf:{},dsfdasfsdf:{},:sdflldsfs{}}}
+	// JSON array methods map filter sort... [{},{},{}]
+	// Vendor   API/SDK to get the data and parse the data.
+	const payload = await getToDoItems()
+	const tasks = Object.entries(payload.todos)
+
 	return (
 		<>
 			<header className="text-center pt-28">
 				<h2 className="text-5xl text-slate-900">Demo Page Component </h2>
 			</header>
 			<main className=" max-w-md mx-auto  py-24">
-				<ToDoList>
-					<ToDoListItem>to do item here</ToDoListItem>
+				<ToDoList className=" space-y-4 p-4 h-[478px] max-h-[478px] overflow-y-scroll shadow-sm rounded-md border border-neutral-200">
+					{tasks.map((task) => (
+						<ToDoListItem key={task[0]} />
+					))}
 				</ToDoList>
 				<AddTask />
 			</main>
--- components/todos/ToDoListItem.jsx
+++ components/todos/ToDoListItem.jsx
@@ -1,7 +1,18 @@
 import {cn} from '@/lib/utils/mergeCss'
 
-function ToDoListItem({children, className}) {
-	return <aside className={cn('bg-neutral-100 flex justify-between border border-black py-2.5 px-3', className)}>{children}</aside>
+function ToDoListItem({children, className, uid}) {
+	return (
+		<aside className={cn('odd:bg-neutral-50 even:bg-gray-100 flex justify-between   py-4 px-3 rounded', className)}>
+			<div>
+				<p className="text-xs text-neutral-400">Category</p>
+				<p className="text-sm text-neutral-600">To Do Item</p>
+			</div>
+			<div className="flex flex-col gap-y-1 text-[10px] items-center">
+				<button className="border border-neutral-500 text-neutral-600 px-2 py-0.5 rounded">Delete</button>
+				<button className="bg-neutral-500 text-white px-2 py-0.5 rounded w-full">Edit</button>
+			</div>
+		</aside>
+	)
 }
 
 export {ToDoListItem}
